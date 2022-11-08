bda99263ed60b5735e2591da7bd8970e5f22f963
bnfreedeveloper <bn.freedeveloper@gmail.com>
2022-11-08 15:02:58 +0100

    loginauth

--- angularfront/src/app/components/login/login.component.ts
+++ angularfront/src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { catchError, delay, tap } from 'rxjs';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { LoginResponse } from "../../Models/loginResponse"
+import { CoreResponse } from "../../Models/coreResponse"
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  form!: FormGroup;
+  loginResponse !: LoginResponse;
+  loading!: boolean;
+  message!: any;
+  get FormValues() {
+    return this.form.controls;
   }
+  constructor(private authService: AuthenticationService, private fb: FormBuilder) { }
+
+  onSubmit() {
+    this.authService.login(this.form.value).pipe(tap(() => this.loading = true),
+      catchError(err => {
+        throw err;
+      }), delay(1500)).subscribe({
+        next: response => {
+          this.loginResponse = response;
+          console.log(this.loginResponse);
+          this.loading = false;
+          this.form.reset();
+        },
+        error: err => {
+          this.loginResponse = { success: false, message: err.error.error } as LoginResponse
+          this.form.reset();
+          this.loading = false;
+          this.message = { color: "red" }
+
+        },
+        complete: () => {
+          this.message = { color: this.loginResponse?.success ? "green" : "red" }
+        }
+      })
+  }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      "username": ["", Validators.required],
+      "password": ["", Validators.required]
+    })
+  }
+
 
 }
